bde6236c0c1ae011103a585e6a6b86f33f6dbe57
ebadamZA <eadam@varsitycollege.co.za>
2022-10-25 11:25:22 +0200

    Add project files.

---
+++ CustomDictionary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CustomDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<int, User> persons = new Dictionary<int, User>();
+            persons.Add(92110, new User(92110, "John", "Doe", 5));
+            persons.Add(21111, new User(21111, "Jane", "Doe", 4));
+            persons.Add(51011, new User(51011, "Sam", "Doe", 3));
+            persons.Add(37289, new User(37289, "Pat", "Doe", 5));
+            persons.Add(5678, new User(5678, "Joe", "Doe", 1));
+            
+            foreach (KeyValuePair<int,User> kv in persons)
+            {
+                Console.WriteLine(kv.Key + " - " + kv.Value.FirstName);
+            }
+            
+            Console.WriteLine("User at index 5678: " + persons[5678].FirstName);
+            
+            persons.Remove(5678);
+            Console.WriteLine("Removed");
+
+            foreach (KeyValuePair<int, User> kv in persons)
+            {
+                Console.WriteLine(kv.Key + " - " + kv.Value.FirstName);
+            }
+            
+            SimpleDictionary<int,string> simpleDictionary = new SimpleDictionary<int, string>();
+
+            simpleDictionary.Add(92110, "John");
+            simpleDictionary.Add(21111, "Jane");
+            simpleDictionary.Add(51011, "Sam");
+            simpleDictionary.Add(37289, "Pat");
+            simpleDictionary.Add(5678, "Joe");
+            
+            simpleDictionary[5679] = "Ron";
+
+            Console.WriteLine(simpleDictionary.ToString());
+            
+            Console.Write("Trying to remove key 2993 : ");
+            Console.WriteLine(simpleDictionary.Remove(2993));
+            
+            Console.Write("Trying to remove key 5679 : ");
+            Console.WriteLine(simpleDictionary.Remove(5679));
+            
+            Console.WriteLine("Dictionary:");
+            
+            Console.WriteLine(simpleDictionary);
+            
+            Console.WriteLine("Retreiving key 5678");
+            Console.WriteLine(simpleDictionary[5678]);
+            
+            Console.WriteLine("sorting...");
+            simpleDictionary.Sort();
+
+            Console.WriteLine("Dictionary sorted by key:");
+            Console.WriteLine(simpleDictionary);
+        }
+    }
+}
---
+++ CustomDictionary/SimpleDictionary.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CustomDictionary
+{
+    class SimpleDictionary<K, V>
+    {
+        List<K> keys = new List<K>();
+        List<V> values = new List<V>();
+
+        public void Add(K key, V value)
+        {
+            if (key == null)
+            {
+                throw new ArgumentException("Key cannot be null");
+            }
+            
+            if (!keys.Contains(key))
+            {
+                keys.Add(key);
+                values.Add(value);                
+            } else
+            {
+                throw new ArgumentException("An item with the same key has already been added.");
+            }            
+        }
+
+        public bool Remove(K key)
+        {
+            if (keys.Contains(key))
+            {
+                values.RemoveAt(keys.IndexOf(key));
+                keys.Remove(key);
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            keys.Clear();
+            values.Clear();
+        }
+
+        public int Size()
+        {
+            return keys.Count();
+        }
+                
+        public V this[K key]
+        {
+            get
+            {
+                if (keys.Contains(key))
+                    return values[keys.IndexOf(key)];
+                else
+                    throw new KeyNotFoundException("Key does not exist");
+            }
+
+            set
+            {
+                if (keys.Contains(key))
+                    values[keys.IndexOf(key)] = value;
+                else
+                    Add(key, value);
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            for (int i = 0; i < keys.Count(); i++)
+            {
+                s += "[Key: " + keys[i] + ", Value: " + values[i] + "]\n";
+            }
+            return s;
+        }
+
+        public void Sort()
+        {
+            var keyArray = keys.ToArray();
+            var valueArray = values.ToArray();
+            
+            Array.Sort(keyArray, valueArray);
+
+            keys = new List<K>(keyArray);
+            values = new List<V>(valueArray);
+        }
+    }
+}
